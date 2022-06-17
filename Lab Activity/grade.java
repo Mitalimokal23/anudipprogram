147210966e8d29d50ce180d9508a8ebf52085c83
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-17 20:31:48 +0530

    Create grade.java

---
+++ Lab Activity/grade.java
@@ -0,0 +1,15 @@
+package java2;
+class Dae{
+    public static void main(String[] args) {
+     
+       {
+		
+		char a = 'b';
+		a = (char)(a + 8);
+		System.out.println(a);
+		a = (char)(a- 8);
+		System.out.println(a);
+
+
+    }
+}
