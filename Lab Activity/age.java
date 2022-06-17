e78f984cac00173f948b89ebbd97a58afc8646f5
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-17 20:31:17 +0530

    Create age.java

---
+++ Lab Activity/age.java
@@ -0,0 +1,29 @@
+package java2;
+public class Dfd{
+
+	public static void main(String[] args) {
+		int age=21;
+		if(age>1 && age<11) {
+			System.out.println("childhood");
+		}
+		else if(age>10 && age<=16) {
+			System.out.println("Adolescence");
+		}
+		else if(age>16 && age<=19) {
+			System.out.println("Teenager");
+		}
+		else if(age>19 && age<=24) {
+			System.out.println("Young adult");
+		}
+		else if(age>24 && age<=35) {
+			System.out.println("Adult");
+		}
+		else if(age>35 && age<=45) {
+			System.out.println("Middle age");
+		}
+		else if(age>45 && age<=60) {
+			System.out.println("Lateadult");
+		}
+		}
+
+
