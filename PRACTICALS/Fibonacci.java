ba58f838040ee5eb557710ef28a8d27516b8909b
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-21 16:21:47 +0530

    Add files via upload

---
+++ PRACTICALS/Fibonacci.java
@@ -0,0 +1,19 @@
+
+2. Write a program to generate fibonacci series upto first 10 terms.
+
+class Fibonacci{
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
