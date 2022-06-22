9d9e72154c985596fa3d88a0cff15211ad99bfbc
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 18:11:20 +0530

    Add files via upload

---
+++ PRACTICALS/ElseIfExample.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String args[])
+{
+ int number = 4;
+ if (number<0)
+{
+System.out.println("number is positive");
+}
+else if (number <= 0)
+{
+System.out.println("number is negative");
+}
+else 
+{
+System.out.println("number is ");
+}
+}
+}
