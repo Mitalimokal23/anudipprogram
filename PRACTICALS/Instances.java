02763635a406268899788c89f3f4f61ccaa2d6f2
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 17:20:54 +0530

    Add files via upload

---
+++ PRACTICALS/Instances.java
@@ -0,0 +1,14 @@
+class Instances{
+  
+     int bs=172;  
+    public static void main(String[] args)
+    {
+       
+      System.out.println( "bs");
+    }
+          public void print(){
+
+       System.out.println(bs);
+
+}
+}
