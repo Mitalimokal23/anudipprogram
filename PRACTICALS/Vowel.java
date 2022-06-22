c240e03e4241eb711e2226bceaab65538ea220ba
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 12:00:28 +0530

    Add files via upload

---
+++ PRACTICALS/Vowel.java
@@ -0,0 +1,19 @@
+public class Vowel{
+
+    public static void main(String[] args) {
+
+        char ch = 'i' ;
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+                
+            
+        }
+    }
+}
