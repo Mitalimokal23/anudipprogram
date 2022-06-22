41b89b1a8505850a342785b7e78f41042f7552cb
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 17:40:44 +0530

    Add files via upload

---
+++ PRACTICALS/SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+class Instances{
+  
+    public static void main(String[] args)
+    {
+        float first = 1.20f;  float second = 2.45f;
+     
+    float temporary = first;
+    
+    first= second;
+ second = temporary;
+ System.out.println( "first number = "  + first);
+      System.out.println( "second number = "  + second);
+    
+
+
+
+
+
+}
+}
