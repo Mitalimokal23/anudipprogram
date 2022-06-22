eac2bc87abd721ee55b2684db2a2c1b4f0cb5b01
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 17:04:41 +0530

    Add files via upload

---
+++ PRACTICALS/Ifelse.java
@@ -0,0 +1,17 @@
+public class Hello {
+public static void main (String[] args) {
+  int num=2,  num1=56, num2=67;
+if(num>num1 && num2>num1)
+{
+
+     System.out.println(num2 + "  is greatest number");
+}
+
+
+else
+{
+   System.out.println(num2 + " is not a greatest number");
+
+}
+}
+}
