fca639e2545f9d04ae2085eeb9dc13552c4c9ada
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-22 17:14:46 +0530

    Add files via upload

---
+++ PRACTICALS/StaticVariable.java
@@ -0,0 +1,15 @@
+class StaticVariable{
+  
+    static int bs=12;  
+    public static void main(String[] args)
+    {
+       Elseif ap = new Elseif();
+      ap.anudip();
+      System.out.println("it is static method : " + bs);
+    }
+   public void anudip()
+    {
+       System.out.println("it is  non static method : " + bs);
+
+}
+}
