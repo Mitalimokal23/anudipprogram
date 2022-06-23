cf50ac96098f35b91cb16090c974b2d16f962fae
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-23 12:52:39 +0530

    Add files via upload

---
+++ Lab Activity/AreaOfReactangle.java
@@ -0,0 +1,12 @@
+class AreaOfReactangle {
+ public  static void main(String args[]) {
+ int length = 45;
+int breadth = 67;
+ int area = length * breadth ;
+System.out.println( " area of rectangle : " +  area) ;
+}
+}
+
+
+
+
