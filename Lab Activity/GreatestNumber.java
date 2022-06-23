489dd8f5f95f63b1b8b0fee11ee227fb654c693f
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-23 11:41:04 +0530

    Add files via upload

---
+++ Lab Activity/GreatestNumber.java
@@ -0,0 +1,21 @@
+public class GreatestNumber {
+public static void main (String[] args) {
+  int num=2,  num1=56, num2=67, num3 = 78;
+if(num>num1 && num2>num1)
+{
+
+     System.out.println(num+ "  is greatest number");
+}
+
+
+else if ( num2 <num3 && num>num2)
+{
+   System.out.println(num1 + " is not a greatest number");
+
+}
+else 
+{
+    System.out.println(num3 + " is a greatest number ");
+}
+}
+}
