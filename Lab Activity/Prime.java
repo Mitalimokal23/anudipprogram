5ab0f5a8a8d63739dffd09b95205840b0a48ad80
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-24 11:54:22 +0530

    Add files via upload

---
+++ Lab Activity/Prime.java
@@ -0,0 +1,18 @@
+class Prime{
+
+public static void main (String args[])
+{
+   int num = 24;
+ boolean flag = false;
+ for (int i = 2 ; i<=num/2; i++){
+ if (num% i  == 0){
+ flag = true;
+break;
+}
+}
+if (! flag) 
+System.out.println(num + " is a prime number");
+else
+System.out.println(num + " is not prime number");
+}
+}
