388ce6ac7a832fd946a9fa81670b569b32a8e4fc
Mitalimokal23 <107388080+Mitalimokal23@users.noreply.github.com>
2022-06-28 19:12:02 +0530

    Add files via upload

---
+++ PRACTICALS/Assig.java
@@ -0,0 +1,17 @@
+class Assig{
+public static void main (String args []) {
+int a[ ] = { 1, 2,3,4,5};  
+int b[]= {9,8,7,6,5};   
+ int c [ ] =  new int[a.length];
+
+        for (int i = 0; i < a.length; i++)
+           c[i] = a[i] + b[i];
+           System.out.println(c[i] + " "); 
+
+
+ 
+
+ 
+        
+    }
+}
